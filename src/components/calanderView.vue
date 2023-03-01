af2662bd350a029a994b4516455188fb6823fd1e
Hammouche-Med <mhammouche995@gmail.com>
2023-03-01 14:56:53 +0100

    added calander

---
+++ src/components/calanderView.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import { reactive } from 'vue'
+
+const calendarOptions = reactive({
+  plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+  initialView: 'dayGridMonth',
+  headerToolbar: {
+    left: 'prev,next today',
+    center: 'title',
+    right: 'dayGridMonth,timeGridWeek,timeGridDay'
+  },
+  editable: true,
+  selectable: true,
+  selectMirror: true,
+  dayMaxEvents: true,
+  weekends: true,
+  select: (event: any) => {
+    console.log(event)
+  },
+  events: [
+    { title: 'event 1', date: '2023-03-01' },
+    { title: 'event 2', date: '2023-03-02' }
+  ]
+})
+</script>
+<template>
+  <FullCalendar :options="calendarOptions" />
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,27 @@
 <script setup lang="ts">
 import AddEvent from '@/components/addEvent.vue'
 import eventList from '@/components/eventList.vue'
+import calanderView from '@/components/calanderView.vue'
 </script>
 
 <template>
   <div class="container-fluid">
     <div class="row">
-      <div class="col-md-4 m-2">
-        <div class="card m-4">
+      <div class="col-md-3">
+        <div class="card mt-2">
           <div class="card-body">
             <AddEvent />
             <eventList />
           </div>
         </div>
       </div>
-      <div class="col-md-8"></div>
+      <div class="col-md-9">
+        <div class="card mt-2">
+          <div class="card-body">
+            <calanderView />
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
